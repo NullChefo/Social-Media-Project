75f2a0cfcb43ef3aa17541f6565160788ad1223d
nullchefo <stefan.kehayov.96@gmail.com>
2023-09-13 19:09:23 +0300

    enable railway 2

--- back-end/authorization-service/src/main/java/com/nullchefo/authorizationservice/utils/PopulateDefaultsUtil.java
+++ back-end/authorization-service/src/main/java/com/nullchefo/authorizationservice/utils/PopulateDefaultsUtil.java
@@ -1,6 +1,7 @@
 package com.nullchefo.authorizationservice.utils;
 
 import com.nullchefo.authorizationservice.service.JpaRegisteredClientRepository;
+
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
@@ -15,174 +16,180 @@ import java.util.List;
 @Service
 public class PopulateDefaultsUtil {
 
-    private final JpaRegisteredClientRepository jpaRegisteredClientRepository;
+	private final JpaRegisteredClientRepository jpaRegisteredClientRepository;
 
-    public PopulateDefaultsUtil(final JpaRegisteredClientRepository jpaRegisteredClientRepository) {
-        this.jpaRegisteredClientRepository = jpaRegisteredClientRepository;
-    }
+	public PopulateDefaultsUtil(final JpaRegisteredClientRepository jpaRegisteredClientRepository) {
+		this.jpaRegisteredClientRepository = jpaRegisteredClientRepository;
+	}
 
-    public void PopulateDefaults() {
+	public void PopulateDefaults() {
 
-        RegisteredClient registeredClient = RegisteredClient.withId(
-                        //	UUID.randomUUID().toString()
-                        "angular-client"
-                )
-                // gateway		this.gatewayClientId
-                .clientId("angular-client")
-                .clientSecret("{noop}secret")
-                // secret		this.gatewayClientSecret
-                //		.clientSecret("{noop}secret")
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-                .redirectUri("http://127.0.0.1:4200")
-                .redirectUri("http://127.0.0.1:4200/auth/authorized")
-                .redirectUri("http://127.0.0.1:4200/silent-renew.html")
-                //		.redirectUri("http://127.0.0.1:3000/authorized")
-                .redirectUri(
-                        "https://diploma-project.nullchefo.com/auth/authorized")
-                .redirectUri(
-                        "https://diploma-project.nullchefo.com/silent-renew.html")
-                .redirectUri("https://diploma-project.nullchefo.com")
+		RegisteredClient registeredClient = RegisteredClient.withId(
+																	//	UUID.randomUUID().toString()
+																	"angular-client"
+																   )
+															// gateway		this.gatewayClientId
+															.clientId("angular-client")
+															.clientSecret("{noop}secret")
+															// secret		this.gatewayClientSecret
+															//		.clientSecret("{noop}secret")
+															.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+															.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+															.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+															.redirectUri("http://127.0.0.1:4200")
+															.redirectUri("http://127.0.0.1:4200/auth/authorized")
+															.redirectUri("http://127.0.0.1:4200/silent-renew.html")
+															//		.redirectUri("http://127.0.0.1:3000/authorized")
+															.redirectUri(
+																	"https://diploma-project.nullchefo.com/auth/authorized")
+															.redirectUri(
+																	"https://diploma-project.nullchefo.com/silent-renew.html")
+															.redirectUri("https://diploma-project.nullchefo.com")
 
-				.redirectUri("https://social-media-project.nullchefo.com/auth/authorized")
-				.redirectUri("https://social-media-project.nullchefo.com")
-				.redirectUri("https://social-media-project.nullchefo.com/silent-renew.html")
+															.redirectUri(
+																	"https://social-media-project.nullchefo.com/auth/authorized")
+															.redirectUri("https://social-media-project.nullchefo.com")
+															.redirectUri(
+																	"https://social-media-project.nullchefo.com/silent-renew.html")
 
+															.redirectUri(
+																	"https://social-media-project-java.nullchefo.com/auth/authorized")
+															.redirectUri(
+																	"https://social-media-project-java.nullchefo.com")
+															.redirectUri(
+																	"https://social-media-project-java.nullchefo.com/silent-renew.html")
+															.redirectUri(
+																	"https://authorization.social-media-project-java.nullchefo.com")
 
-				.redirectUri("https://social-media-project-java.nullchefo.com/auth/authorized")
-				.redirectUri("https://social-media-project-java.nullchefo.com")
-				.redirectUri("https://social-media-project-java.nullchefo.com/silent-renew.html")
-				.redirectUri("https://authorization.social-media-project-java.nullchefo.com")
+															.redirectUri(
+																	"https://diploma-project-ui.nullchefo.com/auth/authorized")
+															.redirectUri(
+																	"https://diploma-project-ui.nullchefo.com/silent-renew.html")
+															.redirectUri("https://diploma-project-ui.nullchefo.com")
 
-				.redirectUri(
-                        "https://diploma-project-ui.nullchefo.com/auth/authorized")
-                .redirectUri(
-                        "https://diploma-project-ui.nullchefo.com/silent-renew.html")
-                .redirectUri("https://diploma-project-ui.nullchefo.com")
+															.redirectUri("https://backend.nullchefo.com/**")
+															.redirectUri(
+																	"https://backend.nullchefo.com/webjars/swagger-ui/4.18.1/oauth2-redirect.html")
+															.redirectUri(
+																	"https://ui-social-media-project-java-railway.nullchefo.com/silent-renew.html")
+															.redirectUri(
+																	"https://ui-social-media-project-java-railway.nullchefo.com/auth/authorized")
+															.redirectUri(
+																	"https://ui-social-media-project-java-railway.nullchefo.com")
 
-                .redirectUri("https://backend.nullchefo.com/**")
-                .redirectUri(
-                        "https://backend.nullchefo.com/webjars/swagger-ui/4.18.1/oauth2-redirect.html")
-															.redirectUri("https://ui-social-media-project-java-railway.nullchefo.com/silent-renew.html")
-															.redirectUri("https://ui-social-media-project-java-railway.nullchefo.com/auth/authorized")
-															.redirectUri("https://ui-social-media-project-java-railway.nullchefo.com")
+															.tokenSettings(TokenSettings
+																				   .builder().accessTokenTimeToLive(
+																			Duration.ofDays(1)).build())
+															.scopes(scopes -> scopes.addAll(List.of(
+																	OidcScopes.OPENID,
+																	OidcScopes.PROFILE,
+																	OidcScopes.EMAIL,
+																	"user.read",
+																	"user.write",
+																	"offline_access"
+																								   )))
+															.clientSettings(ClientSettings
+																					.builder()
+																					.requireProofKey(true) // PKCE
+																					.requireAuthorizationConsent(false)
+																					// This way 1 token is limited to 1 or more resources insted of multiple ones
+																					//																   .setting("settings.client.allowed.resources", allowedResources)
+																					.build())
+															.build();
 
+		jpaRegisteredClientRepository.save(registeredClient);
 
+		registeredClient = RegisteredClient.withId(
+												   //	UUID.randomUUID().toString()
+												   "swagger-client"
+												  )
+										   // gateway		this.gatewayClientId
+										   .clientId("swagger-client")
+										   .clientSecret("{noop}very_secure_OwO_secret")
+										   // secret		this.gatewayClientSecret
+										   //		.clientSecret("{noop}secret")
+										   .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+										   .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_JWT)
+										   .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+										   .authorizationGrantType(AuthorizationGrantType.JWT_BEARER)
+										   .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+										   .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
 
-                .tokenSettings(TokenSettings
-                        .builder().accessTokenTimeToLive(
-                                Duration.ofDays(1)).build())
-                .scopes(scopes -> scopes.addAll(List.of(
-                        OidcScopes.OPENID,
-                        OidcScopes.PROFILE,
-                        OidcScopes.EMAIL,
-                        "user.read",
-                        "user.write",
-                        "offline_access"
-                )))
-                .clientSettings(ClientSettings
-                        .builder()
-                        .requireProofKey(true) // PKCE
-                        .requireAuthorizationConsent(false)
-                        // This way 1 token is limited to 1 or more resources insted of multiple ones
-                        //																   .setting("settings.client.allowed.resources", allowedResources)
-                        .build())
-                .build();
+										   .redirectUri(
+												   "http://127.0.0.1:8092/social-media/swagger/swagger-ui/index.html")
 
-        jpaRegisteredClientRepository.save(registeredClient);
+										   .redirectUri(
+												   "http://127.0.0.1:8000/webjars/swagger-ui/4.18.1/oauth2-redirect.html")
+										   .redirectUri("https://backend.nullchefo.com/**")
+										   .redirectUri(
+												   "https://backend.nullchefo.com/webjars/swagger-ui/4.18.1/oauth2-redirect.html")
 
-        registeredClient = RegisteredClient.withId(
-                        //	UUID.randomUUID().toString()
-                        "swagger-client"
-                )
-                // gateway		this.gatewayClientId
-                .clientId("swagger-client")
-                .clientSecret("{noop}very_secure_OwO_secret")
-                // secret		this.gatewayClientSecret
-                //		.clientSecret("{noop}secret")
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_JWT)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .authorizationGrantType(AuthorizationGrantType.JWT_BEARER)
-                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+										   .tokenSettings(TokenSettings
+																  .builder().accessTokenTimeToLive(
+														   Duration.ofDays(1)).build())
+										   .scopes(scopes -> scopes.addAll(List.of(
+												   OidcScopes.OPENID,
+												   OidcScopes.PROFILE,
+												   OidcScopes.EMAIL,
+												   // TODO remove
+												   "social_media.read",
+												   "social_media.write"
+												   , "gateway.read",
+												   "gateway.write",
+												   "user.read",
+												   "user.write",
+												   "offline_access"
+																				  )))
+										   .clientSettings(ClientSettings
+																   .builder()
 
-                .redirectUri(
-                        "http://127.0.0.1:8092/social-media/swagger/swagger-ui/index.html")
+																   .requireProofKey(true) // PKCE
+																   .requireAuthorizationConsent(true)
+																   .build())
+										   .build();
 
-                .redirectUri(
-                        "http://127.0.0.1:8000/webjars/swagger-ui/4.18.1/oauth2-redirect.html")
-                .redirectUri("https://backend.nullchefo.com/**")
-                .redirectUri(
-                        "https://backend.nullchefo.com/webjars/swagger-ui/4.18.1/oauth2-redirect.html")
+		jpaRegisteredClientRepository.save(registeredClient);
 
-                .tokenSettings(TokenSettings
-                        .builder().accessTokenTimeToLive(
-                                Duration.ofDays(1)).build())
-                .scopes(scopes -> scopes.addAll(List.of(
-                        OidcScopes.OPENID,
-                        OidcScopes.PROFILE,
-                        OidcScopes.EMAIL,
-                        // TODO remove
-                        "social_media.read",
-                        "social_media.write"
-                        , "gateway.read",
-                        "gateway.write",
-                        "user.read",
-                        "user.write",
-                        "offline_access"
-                )))
-                .clientSettings(ClientSettings
-                        .builder()
+		registeredClient = RegisteredClient.withId(
+												   "spring-client-oidc"
+												  )
+										   .clientId("spring")
+										   .clientSecret("{noop}very_secure-spring!!")
+										   .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+										   .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_JWT)
+										   .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+										   .authorizationGrantType(AuthorizationGrantType.JWT_BEARER)
+										   .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+										   .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
 
-                        .requireProofKey(true) // PKCE
-                        .requireAuthorizationConsent(true)
-                        .build())
-                .build();
+										   .redirectUri("http://127.0.0.1:8082/login/oauth2/code/spring-client-oidc")
+										   .redirectUri("http://127.0.0.1:8082/authorized")
 
-        jpaRegisteredClientRepository.save(registeredClient);
+										   .redirectUri("http://user:8082/login/oauth2/code/spring-client-oidc")
+										   .redirectUri("http://user:8082/authorized")
 
-        registeredClient = RegisteredClient.withId(
-                        "spring-client-oidc"
-                )
-                .clientId("spring")
-                .clientSecret("{noop}very_secure-spring!!")
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_JWT)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .authorizationGrantType(AuthorizationGrantType.JWT_BEARER)
-                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+										   .tokenSettings(TokenSettings.builder().accessTokenTimeToLive(
+												   Duration.ofDays(1)).build())
+										   .scopes(scopes -> scopes.addAll(List.of(
+												   OidcScopes.OPENID,
+												   OidcScopes.PROFILE,
+												   OidcScopes.EMAIL,
+												   // TODO remove
+												   "social_media.read",
+												   "social_media.write"
+												   , "gateway.read",
+												   "gateway.write",
+												   "user.read",
+												   "user.write",
+												   "mail_process.read",
+												   "mail_process.write",
+												   "mail_send.read",
+												   "mail_send.write",
+												   "offline_access"
+																				  )))
+										   .build();
 
-                .redirectUri("http://127.0.0.1:8082/login/oauth2/code/spring-client-oidc")
-                .redirectUri("http://127.0.0.1:8082/authorized")
-
-                .redirectUri("http://user:8082/login/oauth2/code/spring-client-oidc")
-                .redirectUri("http://user:8082/authorized")
-
-                .tokenSettings(TokenSettings.builder().accessTokenTimeToLive(
-                        Duration.ofDays(1)).build())
-                .scopes(scopes -> scopes.addAll(List.of(
-                        OidcScopes.OPENID,
-                        OidcScopes.PROFILE,
-                        OidcScopes.EMAIL,
-                        // TODO remove
-                        "social_media.read",
-                        "social_media.write"
-                        , "gateway.read",
-                        "gateway.write",
-                        "user.read",
-                        "user.write",
-                        "mail_process.read",
-                        "mail_process.write",
-                        "mail_send.read",
-                        "mail_send.write",
-                        "offline_access"
-                )))
-                .build();
-
-        jpaRegisteredClientRepository.save(registeredClient);
+		jpaRegisteredClientRepository.save(registeredClient);
 
 
 
@@ -367,6 +374,6 @@ public class PopulateDefaultsUtil {
 
 		*/
 
-    }
+	}
 
 }
